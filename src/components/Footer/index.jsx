6c5ba758d8f3c56274616669b0d07bb8ec12926d
Barbora Tesařová <tesarovabara@seznam.cz>
2024-04-27 10:25:07 +0200

    Přidání komponentů

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export const Footer = (props) => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer__content">
+          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+        </div>
+      </div>
+    </footer>
+  );
+};
